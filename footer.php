33335c72907653713550fd42e33097e3b765d3e4
Keanan Koppenhaver <keanan@alphaparticle.com>
2022-10-29 17:26:11 -0500

    Move bootstrap to footer to load after HTML

--- footer.php
+++ footer.php
@@ -28,6 +28,9 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
 <?php wp_footer(); ?>
 </body>
 </html>
