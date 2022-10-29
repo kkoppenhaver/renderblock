4b6ac65b0224ff3d00e35afd80f90984b0deb7e8
Keanan Koppenhaver <keanan@alphaparticle.com>
2022-10-29 17:25:46 -0500

    Add bootstrap CSS in header

--- header.php
+++ header.php
@@ -17,6 +17,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
 	<?php wp_head(); ?>
 </head>
 
